51762c6beb279bb20b0ccf7d53221441a936df57
Dan Nohr <danielnohr@gmail.com>
2020-01-21 20:30:11 -0600

    update

--- scrapers/scraper.js
+++ scrapers/scraper.js
@@ -174,7 +174,7 @@ async function scrapeOnDemandRead(lastDataDate) {
       .waitForSelector(buttonSelector)
       .then(console.log("button is here"));
 
-    // await page.click(buttonSelector, { clickCount: 1 });
+    await page.click(buttonSelector, { clickCount: 1 });
 
     await page
       .waitForSelector(dateSelector)
